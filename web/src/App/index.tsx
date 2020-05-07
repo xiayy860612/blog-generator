c24dcd31f63c1b60ee48ada78989658f99d01a8b
Yangyang <xyy@conti-app-ma-086.local>
2020-05-07 17:28:57 +0800

    generate sidebar

--- web/src/App/index.tsx
+++ web/src/App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
+
 import './index.css';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
@@ -15,31 +15,12 @@ import {
   VideoCameraOutlined,
   UploadOutlined,
 } from '@ant-design/icons';
+import AppIndex from '../AppIndex';
+import UserInfoCard from '../UserInfo';
+import MenuList from '../Menu';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
-
-
-const AppIndex: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 class App extends React.Component {
   state = {
@@ -49,43 +30,33 @@ class App extends React.Component {
   onCollapse = (collapsed: boolean) => {
     this.setState({ collapsed });
   };
-
+  
   render() {
+    let userElement = <UserInfoCard />;
+    if (this.state.collapsed) {
+      userElement = <div />;
+    }
     return (
       <Router>
         <Layout>
-          <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} style={{
-        overflow: 'auto',
-        height: 'auto',
-        position: 'fixed',
-      }}>
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">
-                <UserOutlined />
-                <span>nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <VideoCameraOutlined />
-                <span>nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <UploadOutlined />
-                <span>nav 3</span>
-              </Menu.Item>
-            </Menu>
+          <Sider
+            collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} 
+            style={{
+              overflow: 'auto',
+              position: 'fixed',
+              height: '100%'
+            }}
+          >
+            {userElement}
+            <MenuList />
           </Sider>
           <Layout className="site-layout">
-          <Content className="site-layout-background">
-              <Switch>
-                <Route path="/" exact={true} component={AppIndex} />
-              </Switch>
-            </Content>
             <Content className="site-layout-background">
               <Switch>
                 <Route path="/" exact={true} component={AppIndex} />
               </Switch>
             </Content>
+            <Footer>Footer</Footer>
           </Layout>
         </Layout>
       </Router>
